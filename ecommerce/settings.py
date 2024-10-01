284935e7d69d6958c371e2a8d3e11a88d1803393
MTR-BRAND <mahmudaarafat0077@gmail.com>
2024-10-01 23:04:24 +0600

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -54,10 +54,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
